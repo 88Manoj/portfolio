e256e6c0504d089ff0776caff15c942286dd397a
88Manoj <144885057+88Manoj@users.noreply.github.com>
2024-06-05 16:09:10 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+// var typed= new Typed(".text",{
+//     String:["Frontend Devloper","youtuber","Web Development"],  
+//     typespeed:100,
+//     backspeed:100,
+//     backdelay:1000,
+//     loop:true
+// });
+
+var typed = new Typed('.text', {
+    strings: ['Frontend Devloper' , 'Heacker.'],
+    typeSpeed: 100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+  });
